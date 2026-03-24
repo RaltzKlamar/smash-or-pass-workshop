82a33d411f3947a86dcc4f9eae2608aa6f0fe61a
Raltz Klamar <raltzklamar@gmail.com>
2026-03-24 07:23:06 +0000

    fix apiTestRun again

--- apiTestRun.js
+++ apiTestRun.js
@@ -1,5 +1,6 @@
 import * as fetchImages from "./util/fetchImages.ts";
 
-if (fetchImages && typeof fetchImages === "function") {
-    fetchImages();
+if (fetchImages?.default && typeof fetchImages.default === "function") {
+    const data = await fetchImages.default();
+    console.log(data);
 }
